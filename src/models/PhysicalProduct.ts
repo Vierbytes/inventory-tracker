ed2374a08a6abbf97fb0365bc929cce014d0e9d2
Vierbytes <corey@vierbytes.com>
2025-11-21 05:24:02 -0500

    Task 3 Completed. PhysicalProduct class that extends Product created.

---
+++ src/models/PhysicalProduct.ts
@@ -0,0 +1,22 @@
+import { Product } from "./Product.js"
+
+export class PhysicalProduct extends Product {
+    private weight: number
+
+    constructor(sku: string, name: string, price: number, weight: number) {
+        super(sku, name, price)
+        this.weight = weight
+    }
+
+    override getPriceWithTax(): number {
+        return this.price * 1.10 // 10% tax
+    }
+
+    get weightInKg(): string {
+        return `${this.weight} kg`
+    }
+
+    displayDetails(): string {
+        return `${super.displayDetails()}, Weight: ${this.weightInKg}`
+    }
+}
